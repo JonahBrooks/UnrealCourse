0c4ddb2bf44289fb7cf6b8af8476d766ca1cc94c
Jonah Brooks <brooks.jonah.c@gmail.com>
2017-08-13 21:15:41 -0700

    BE28 84 Accessors & Memory Layout

--- 03BuildingEscape/BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ 03BuildingEscape/BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -39,6 +39,7 @@ void UGrabber::BeginPlay()
 	if (InputComponent)
 	{
 		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+		InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::Release);
 	}
 	else
 	{
@@ -50,6 +51,12 @@ void UGrabber::BeginPlay()
 void UGrabber::Grab()
 {
 	UE_LOG(LogTemp, Warning, TEXT("Grab key pressed!"));
+}
+
+void UGrabber::Release()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Grab key released!"));
+
 }
 
 // Called every frame
--- 03BuildingEscape/BuildingEscape/Source/BuildingEscape/Grabber.h
+++ 03BuildingEscape/BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -39,4 +39,7 @@ private:
 
 	// Ray-cast and grab what's in reach
 	void Grab();
+
+	// Called when grab is released
+	void Release();
 };
