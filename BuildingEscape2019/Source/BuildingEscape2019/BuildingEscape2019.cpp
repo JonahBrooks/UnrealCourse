eafbec1af1a0545cbfcf1339f0adffd98c8e7f92
Jonah Brooks <brooks.jonah.c@gmail.com>
2020-06-07 22:50:12 -0700

    Initial commit for BuildingEscape2019

---
+++ BuildingEscape2019/Source/BuildingEscape2019/BuildingEscape2019.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BuildingEscape2019.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BuildingEscape2019, "BuildingEscape2019" );
---
+++ BuildingEscape2019/Source/BuildingEscape2019/BuildingEscape2019GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingEscape2019GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPE2019_API ABuildingEscape2019GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
