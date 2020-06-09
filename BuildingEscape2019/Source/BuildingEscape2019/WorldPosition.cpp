ccc20379e9e300ab3d762e24e6791c2c7ef8a4e4
Jonah Brooks <brooks.jonah.c@gmail.com>
2020-06-08 21:39:26 -0700

    Watched through video "Accessing An Object's Name"

---
+++ BuildingEscape2019/Source/BuildingEscape2019/WorldPosition.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldPosition.h"
+//#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UWorldPosition::UWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+
+	//UE_LOG(LogTemp, Display, TEXT("This is a log message."));
+	//UE_LOG(LogTemp, Warning, TEXT("This is a warning message."));
+	//UE_LOG(LogTemp, Error, TEXT("This is an error message."));
+
+	//FString Log = TEXT("Hello!");
+	//UE_LOG(LogTemp, Warning, TEXT("%s"), *Log);
+	//FString PtrLog = &Log;
+	//UE_LOG(LogTemp, Warning, TEXT("%s"), **PtrLog);
+
+	FString Name = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Object name is: %s"), *Name);
+}
+
+
+// Called every frame
+void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
